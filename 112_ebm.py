ee169db3b2922527f1898d76c78e53059b5f1783
Duncan Fun <u101530009@gap.kmu.edu.tw>
2023-03-20 22:49:12 +0800

    初版

---
+++ 112_ebm.py
@@ -0,0 +1,38 @@
+import streamlit as st
+from deta import Deta
+import pandas as pd
+from random import randrange
+
+def link_deta(base_name):
+    deta = Deta(st.secrets['DB_TOKEN'])
+    db = deta.Base(base_name)
+    return db
+
+db=link_deta(st.secrets['DB'])
+
+st.set_page_config(page_title='112年實證藥學-抽籤網頁',layout="wide") #修改網頁title，並預設為寬廣模式
+st.markdown('## 112年實證藥學-抽籤網頁') 
+
+st.markdown('---') 
+st.markdown('### 抽點~')
+
+if st.button('抽'):
+    while True:
+        r=randrange(0,60)
+        print(r)
+        who=db.fetch({'key':str(r),'1120321?ne':'OK'})
+        who=who.items
+        print(who)
+        if len(who)==1:
+            who=who[0]
+            break
+    st.markdown('#### '+'學號：'+str(who['學號']))
+    st.markdown('#### '+'班級：'+who['學生班級'])
+    st.markdown('#### '+'姓名：'+who['姓名'])
+    st.markdown('---') 
+    updates={"1120321":'OK'}
+    db.update(updates, str(r))
+else:
+    st.write('尚未抽出')
+
+
